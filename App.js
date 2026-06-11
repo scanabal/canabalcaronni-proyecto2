5505430727c131b040d927e3dd6f688fff7764da
icaronni.ux <icaronni@udesa.edu.ar>
2026-06-10 23:09:44 -0300

    hice la navegacion tab

--- App.js
+++ App.js
@@ -4,24 +4,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login/Login';
 import Register from './src/screens/Register/Register';
-// import HomeMenu from './src/components/HomeMenu/HomeMenu';
+import HomeMenu from './src/components/HomeMenu/HomeMenu';
 
 const Stack = createNativeStackNavigator()
 
-export default function App() {
+function App() {
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={ Login } />
-         <Stack.Screen name="Register" component={ Register } /> 
-        {/* <Stack.Screen name="HomeMenu" component={ HomeMenu } /> */}
+        <Stack.Screen name="Login" component={ Login } options={ {headerShown: false} }/>
+        <Stack.Screen name="Register" component={ Register } options={ {headerShown: false} }/> 
+        <Stack.Screen name="HomeMenu" component={ HomeMenu } options={ {headerShown: false} }/> 
       </Stack.Navigator>
 
     </NavigationContainer>
   )
-    
-  
 }
+
+export default App;
 
 
--- src/components/HomeMenu/HomeMenu.js
+++ src/components/HomeMenu/HomeMenu.js
@@ -1,4 +1,30 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-const Tab = createBottomTabNavigator
+import Home from '../../screens/Home/Home';
+import CrearPost from '../../screens/CrearPost/CrearPost';
+import MiPerfil from '../../screens/MiPerfil/MiPerfil';
+import { FontAwesome, Feather, AntDesign } from '@expo/vector-icons'; 
 
-export default HomeMenu
+const Tab = createBottomTabNavigator()
+
+function HomeMenu() {
+    return (
+        <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
+            <Tab.Screen name="Home" component={ Home } 
+                options={
+                    {tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />}
+                    } 
+            />
+            <Tab.Screen name="Crear Post" component={CrearPost} 
+                options={ 
+                    {tabBarIcon: () => <AntDesign name="plus-square" size={24} color="black" /> }
+                    } 
+            />
+            <Tab.Screen name="Mi Perfil" component={MiPerfil} 
+                options={ 
+                    {tabBarIcon: () => <Feather name="user" size={24} color="black" /> }
+                    } 
+            />
+        </Tab.Navigator>
+    );
+}
+export default HomeMenu;
