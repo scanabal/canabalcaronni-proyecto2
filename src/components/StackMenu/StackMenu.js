598f77c1b26b9fc83dfc8b52ea18b28419b554ec
scanabal <scanabal@udesa.edu.ar>
2026-06-15 17:20:21 -0300

    ComentarPost

--- src/components/StackMenu/StackMenu.js
+++ src/components/StackMenu/StackMenu.js
@@ -1,6 +1,6 @@
-import App from "../../../App"
-import Home from "../../screens/Home/Home"
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ComentarPost from '../../screens/ComentarPost/ComentarPost';
+// IMPORTAR HOME
 
 const Stack = createNativeStackNavigator()
 
@@ -8,10 +8,11 @@ function StackMenu(){
     
 return(
     
-       <NavigationContainer>
-            <Stack.Screen name="Login" component= { Login } options={ { headerShown: false} }/>
-            <Stack.Screen name="Register" component= { Register } options={ { headerShown: false} }/>
-        </NavigationContainer>
-)}
+       <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="home" component= { Home } options={ { headerShown: false} }/>
+            <Stack.Screen name="comentarPost" component= { ComentarPost } options={ { headerShown: false} }/>
+        </Stack.Navigator>
+);
+}
 
 export default StackMenu;
---
+++ src/screens/ComentarPost/ComentarPost.js
@@ -0,0 +1,69 @@
+import { auth } from "../../firebase/config";
+import { useState, useEffect } from 'react';
+import { Pressable } from "react-native";
+import { Text, View, TextInput} from "react-native";
+import { ActivityIndicator, FlatList } from "react-native-web";
+
+function ComentarPost(props)
+const [comentario, setComentario] = useState("")
+const [comentarios, setComentarios] = useState([])
+const [post, setPost] = useStete("")
+
+function onSubmit(){
+    db.collection("posts").doc(props.route.params.id).update({
+        comentarios: firebase.firestore.FieldValue.arrayUnion({
+            owner: auth.currentUser.email,
+            texto: comentario
+        })
+    })
+    .then(() => {
+        setComentario("")
+    })
+    .catch(e => {
+        console.log(e);
+    })
+}
+
+useEffect(
+    () => {
+        db.collection("posts").onSnapshot(
+            docs => {
+                let post
+                docs.forEach(doc => {
+                    console.log(doc.data());
+                    if (doc.id == props.route.params.id) {
+                        post = { id: doc.id, data: doc.data () }
+                    }
+                })
+                setPost(post)
+                setComentarios(post.data.comentarios)
+            }
+        )
+    },
+    []
+)
+// return(
+// <View> {/* style={styles.container} */}
+// <Text> {/* style={styles.title} */} Comentar Post</Text>
+// {post.data ? <View> <Text {/* style={styles.texto} */} >{post.data.owner}</Text>
+// <Text {/* style={styles.texto} */}>{post.data.texto}</Text></View> : <ActivityIndicator></ActivityIndicator>
+// <TextInput
+// // style={styles.input}
+// placeholder="agregar un comentario"
+// onChange={text => setComentario(text)}
+// value={comentario}
+// />
+
+// <Pressable onPress={() => onSubmit()}>
+//     {/* <Text style={styles.buton}>Comentar</Text> */}
+// </Pressable>
+// <FlatList
+// data={comentarios}
+// keyExtractor={(item,index) => index.toString()}
+// renderItem={({item}) => (
+//     <View style={}
+// )
+// }
+
+export default ComentarPost
+
